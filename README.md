a70aa7a3a9d2e0523bac20ca275dd1f0a81b53f8
Gustavo <gustavotinho@gmail.com>
2021-01-10 13:56:13 -0300

    Projeto Cálculo Rápido

---
+++ README.md
@@ -0,0 +1,48 @@
+<h1 align = "center">
+    <img src="https://ik.imagekit.io/dz8an0co8lq/Tela_1_VWkSH57Ks.png">
+<h1>
+
+## Sobre o Projeto 
+
+O projeto **Cálculo Rápido** é um aplicativo nativo com o objetivo de gerar operações aritméticas aleatorias para fortalecer o resultado final para o usuario.
+
+## Tecnologias Utilizadas 
+
+O projeto foi desenvolvido com as seguintes tecnologias: 
+
+- [Android Studio](https://developer.android.com) 
+
+- [Java](https://www.java.com/pt-BR/) 
+
+## Como baixar o Projeto
+
+
+```bash 
+
+    # Clonar o repositório 
+    $ git clone https://github.com/ghdss25/CalculoRapido.git
+
+    # Baixar o instalador do Android Studio
+    $ Abrir o Android Studio  
+
+    $ importar o projeto para Ide do Android Studio 
+
+    $ Escolher um emulador de Celular
+
+    $ Escolher a versão do Android que tem que ser 7.0 
+
+    $ Emular esse android Instalado 
+
+    $ Compilar o Projeto já na própria IDE do Android Studio
+```
+
+## Apresentação do Projeto - Cálculo Rápido
+
+<h1 align = "center">
+    <img src="Calculo-Rapido.gif">
+
+---
+
+---
+##Desenvolvido por Gustavo Henrique de Souza Silva
+
--- app/src/main/java/com/example/calculorapido/Operacao4.java
+++ app/src/main/java/com/example/calculorapido/Operacao4.java
@@ -29,7 +29,7 @@ public class Operacao4 extends AppCompatActivity {
         int soma = x + y;
 
         TextView Situacao = findViewById( R.id.Situacao );
-        Situacao.setText( "Soma: " + soma );
+        Situacao.setText( "Soma: " + String.format("%.2f", soma));
     }
 
     public void Calculo_Multiplicar(View v1) {
@@ -45,7 +45,7 @@ public class Operacao4 extends AppCompatActivity {
         int mult = x * y;
 
         TextView Situacao1 = findViewById( R.id.Situacao1 );
-        Situacao1.setText("Multiplica: " + mult );
+        Situacao1.setText( "Multiplicação: " + String.format("%.2f", mult));
     }
 
     public void Calculo_Divisao(View v2) {
@@ -61,7 +61,7 @@ public class Operacao4 extends AppCompatActivity {
         int div = x / y;
 
         TextView Situacao2 = findViewById( R.id.Situacao2 );
-        Situacao2.setText( "Divisão: " + div );
+        Situacao2.setText( "Divisão: " + String.format("%.2f", div));
     }
 
     public void Calculo_Subtracao(View v3) {
@@ -77,6 +77,52 @@ public class Operacao4 extends AppCompatActivity {
         int sub = x - y;
 
         TextView Situacao4 = findViewById( R.id.Situacao4 );
-        Situacao4.setText("Subtração: " + sub );
+        Situacao4.setText("Subtração: " + String.format("%.2f", sub));
+    }
+
+    public void Calculo_Potencia(View v4) {
+
+        Random rand = new Random();
+        double potencia = 0;
+
+        int x = rand.nextInt(10000);
+
+        TextView Conta5 = findViewById(R.id.Conta5);
+        Conta5.setText("Potência de x ao quadrado: " + x);
+
+        potencia = Math.pow(x,2);
+
+        TextView Situacao5 = findViewById( R.id.Situacao5);
+        Situacao5.setText("Resposta: " + String.format("%.2f", potencia));
+    }
+
+    public void Calculo_Raiz_Quadrada(View v5) {
+
+        Random random = new Random();
+
+        int x = random.nextInt(10000);
+
+        TextView Conta6 = findViewById(R.id.Conta6);
+        Conta6.setText("Raiz Quadrada de x é: " + x);
+
+        double rq = Math.sqrt(x);
+
+        TextView Situacao6 = findViewById( R.id.Situacao6);
+        Situacao6.setText("Resposta: " + String.format("%.2f", rq));
+    }
+
+    public void Calculo_Raiz_Cúbica(View v6) {
+
+        Random random = new Random();
+
+        int x = random.nextInt(10000);
+
+        TextView Conta7 = findViewById( R.id.Conta7);
+        Conta7.setText(" Raiz Cúbica de : " + x);
+
+        double rc = Math.cbrt(x);
+
+        TextView Situacao7 = findViewById( R.id.Situacao7);
+        Situacao7.setText("Resposta: " + String.format("%.2f", rc));
     }
 }
